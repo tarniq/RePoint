2585919cd0addc1b7680fd9e5bc4bb3d843b7095
tarniq <thales.cuellas@gmail.com>
2025-10-24 16:55:26 -0300

    first commit

---
+++ src/main/java/com/RePoint/RePointApplication.java
@@ -0,0 +1,13 @@
+package com.RePoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RePointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RePointApplication.class, args);
+	}
+
+}
